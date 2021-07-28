368a2d78c0a1f2484bef2143e2a5556e03637016
Brittney Payne <ohoptional@gmail.com>
2021-07-28 11:43:35 -0700

    refactored user methods to implement composition class methods

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
+import { userInfo } from 'os';
 import { User } from './models/User';
-import axios from 'axios';
-
-// const user = new User({ id: 1 });
 
 
-// const newUser = new User({ name: "Melody" });
+
+// const newUser = new User({ name: "Banana", age: 654 });
 
 
+
+// console.log(newUser.get('name'))
+
+// newUser.on("change", () => { console.log("User was changed") })
+// newUser.set({ name: "oranges" })
+
+
+const user = new User({ id: 1, name: "Hulk Hogan", age: 675 })
+
+user.on('save', () => {
+  console.log(user);
+});
+
+user.save();
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,7 @@
 import { Sync } from './Sync';
 import { Eventing } from './Eventing';
+import { Attributes } from './Attributes';
+import { AxiosPromise, AxiosResponse } from 'axios';
 
 
 
@@ -16,5 +18,48 @@ export class User {
 
   public events: Eventing = new Eventing();
   public sync: Sync<UserProps> = new Sync<UserProps>('http://localhost:3000/users');
+  public attributes: Attributes<UserProps>;
 
+
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
+  }
+
+  get on() {
+    return this.events.on;
+  }
+
+  get trigger() {
+    return this.events.trigger;
+  }
+
+  get get() {
+    return this.attributes.get;
+  }
+
+  set(update: UserProps): void {
+    this.attributes.set(update);
+    this.events.trigger('change');
+  }
+
+  fetch(): void {
+    const id = this.attributes.get('id')
+    if (typeof id !== 'number') {
+      throw new Error('Cannot fetch without ID')
+
+    }
+    this.sync.fetch(id).then((response: AxiosResponse): void => {
+      this.set(response.data);
+    })
+  }
+
+  save(): void {
+    this.sync.save(this.attributes.getAll())
+      .then((response: AxiosResponse): void => {
+        this.trigger('save');
+      })
+      .catch(() => {
+        this.trigger('error');
+      });
+  }
 }
