086e9ca4a659c9d09a98b42392933e1d2335c1ac
Brittney Payne <ohoptional@gmail.com>
2021-07-28 10:20:49 -0700

    refactor to use composition and split classes into modular models

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,10 @@
 import { User } from './models/User';
 import axios from 'axios';
 
-const user = new User({ id: 1 });
+// const user = new User({ id: 1 });
 
 
-// user.fetch();
-
-
-// user.set({ name: "Agent Orange", age: 20 })
-
-// user.save();
-
-
-const newUser = new User({ name: "Melody" });
-
-newUser.save();
-
+// const newUser = new User({ name: "Melody" });
 
 
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,11 @@
-import axios, { AxiosResponse } from 'axios';
+import { Sync } from './Sync';
+import { Eventing } from './Eventing';
 
 
-//type alias for empty function 
-type Callback = () => void;
+
 
 // interface for User attributes 
-interface UserProps {
+export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
@@ -13,47 +13,8 @@ interface UserProps {
 
 
 export class User {
-  events: { [key: string]: Callback[] } = {}
 
-  constructor(private data: UserProps) { }
-
-  get(propName: string): (string | number) {
-    return this.data[propName]
-  }
-  set(update: UserProps): void {
-    Object.assign(this.data, update);
-  }
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback)
-    this.events[eventName] = handlers
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-    handlers.forEach(callback => {
-      callback();
-    });
-
-  }
-  fetch(): void {
-    axios.get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id')
-    if (id) {
-      axios.put(`http://localhost:3000/users/${id}`, this.data)
-    } else {
-      axios.post('http://localhost:3000/users', this.data);
-    }
-  }
+  public events: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>('http://localhost:3000/users');
 
 }
