06fee0a05cdfd383e2f9cc46e708fa4d5998f07f
Brittney Payne <ohoptional@gmail.com>
2021-07-28 12:30:48 -0700

    added model class to utilize inheritance to modularize user class

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,8 @@ import { User } from './models/User';
 
 
 
-// const newUser = new User({ name: "Banana", age: 654 });
 
+const user = User.buildUser({ id: 6 })
 
-
-// console.log(newUser.get('name'))
-
-// newUser.on("change", () => { console.log("User was changed") })
-// newUser.set({ name: "oranges" })
-
-
-const user = new User({ id: 1, name: "Hulk Hogan", age: 675 })
-
-user.on('save', () => {
-  console.log(user);
-});
-
-user.save();
-
+user.fetch();
+console.log(user)
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,10 @@
-import { Sync } from './Sync';
-import { Eventing } from './Eventing';
+import { Model } from './Model';
 import { Attributes } from './Attributes';
-import { AxiosPromise, AxiosResponse } from 'axios';
+import { Eventing } from './Eventing'
+import { ApiSync } from './ApiSync';
 
 
-
+const rootUrl = 'http://localhost:3000/users';
 
 // interface for User attributes 
 export interface UserProps {
@@ -14,52 +14,16 @@ export interface UserProps {
 }
 
 
-export class User {
+export class User extends Model<UserProps> {
 
-  public events: Eventing = new Eventing();
-  public sync: Sync<UserProps> = new Sync<UserProps>('http://localhost:3000/users');
-  public attributes: Attributes<UserProps>;
-
-
-  constructor(attrs: UserProps) {
-    this.attributes = new Attributes<UserProps>(attrs);
+  static buildUser(attrs: UserProps): User {
+    return new User(
+      new Attributes<UserProps>(attrs),
+      new Eventing(),
+      new ApiSync<UserProps>(rootUrl)
+    );
   }
 
-  get on() {
-    return this.events.on;
-  }
-
-  get trigger() {
-    return this.events.trigger;
-  }
-
-  get get() {
-    return this.attributes.get;
-  }
-
-  set(update: UserProps): void {
-    this.attributes.set(update);
-    this.events.trigger('change');
-  }
-
-  fetch(): void {
-    const id = this.attributes.get('id')
-    if (typeof id !== 'number') {
-      throw new Error('Cannot fetch without ID')
-
-    }
-    this.sync.fetch(id).then((response: AxiosResponse): void => {
-      this.set(response.data);
-    })
-  }
-
-  save(): void {
-    this.sync.save(this.attributes.getAll())
-      .then((response: AxiosResponse): void => {
-        this.trigger('save');
-      })
-      .catch(() => {
-        this.trigger('error');
-      });
-  }
 }
+
+
